66d4190f02185597797a8b6aaa9a9546334d6bb2
onoonan <onoonan.ford.com>
2026-07-16 15:25:39 +0100

    largest number question it1

--- src/main/kotlin/com/oocode/Answerer.kt
+++ src/main/kotlin/com/oocode/Answerer.kt
@@ -2,6 +2,15 @@ package com.oocode
 
 class Answerer {
     fun answerFor(question: String): String {
+        //Which of the following numbers is the largest: 15, 57, 36?
+        if (question.startsWith("Which of the following numbers is the largest:")) {
+            var numberStrings = question.substringAfter("Which of the following numbers is the largest:").dropLast(1).trim().split(", ")
+            var numbers = mutableListOf<Int>()
+            for (number in numberStrings) {
+                numbers.add(number.toInt())
+            }
+            return numbers.max().toString()
+        }
         return "oscar"
     }
 }
